148274b545be39e5117cd94a27babd6eedd47b9a
Frenco <hey@frenco.dev>
2021-10-24 23:57:35 +0630

    test: add initial set of tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,62 @@
-import thanosSort from '../src/index'
+/* eslint-disable @typescript-eslint/no-empty-function */
 
-test("the avengers failed (at least iron man don't die :3)", () => {
-  expect(thanosSort([0, 1]).length).toBe(1)
+import {All, Controller, Get, setup as ombudSetup} from '../src'
+import {Router} from '../src/decorators/handlers.decorator'
+import {Keys} from '../src/decorators/keys'
+
+test('@Controller', () => {
+  @Controller('https://example.com')
+  class Proxy {}
+
+  // Replicated `setup` function for tests
+  const setup: typeof ombudSetup = (controller) => {
+    const options = Reflect.getMetadata(Keys.CONTROLLER_OPTIONS, controller)
+    expect(options).toMatchObject({upstream: 'https://example.com'})
+
+    return async () => {}
+  }
+
+  setup(Proxy)
+})
+
+test('@METHOD', () => {
+  @Controller('https://example.com')
+  class Proxy {
+    @Get('/public')
+    async pub() {}
+
+    @Get('/private')
+    @All('/user/:id')
+    async auth() {}
+  }
+
+  // Replicated `setup` function for tests
+  const setup: typeof ombudSetup = (controller) => {
+    const router: Router = Reflect.getMetadata(Keys.ROUTER, controller)
+
+    // routing
+    expect(router.find('GET', '/public')).toEqual({
+      params: {},
+      handlers: ['pub']
+    })
+
+    expect(router.find('GET', '/private')).toEqual({
+      params: {},
+      handlers: ['auth']
+    })
+
+    expect(router.find('GET', '/user/1')).toEqual({
+      params: {id: '1'},
+      handlers: ['auth']
+    })
+
+    expect(router.find('POST', '/user/1')).toEqual({
+      params: {id: '1'},
+      handlers: ['auth']
+    })
+
+    return async () => {}
+  }
+
+  setup(Proxy)
 })
